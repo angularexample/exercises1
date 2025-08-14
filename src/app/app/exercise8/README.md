a8816354762849593aff72bdf143f7bd2f6da8b7
angularexample <info@angularexample.com>
2025-08-14 17:04:46 -0400

    exercise8 CoderByte person form done

---
+++ src/app/app/exercise8/README.md
@@ -0,0 +1,16 @@
+# CoderByte Angular Assessment - Person Change
+
+## Description
+
+The starting code gives you a person object.
+The person object is displayed as JSON in the view.
+
+## The Challenge
+
+Create a form with a field for each property of the person object.
+
+The form should display the person object.
+
+There should be a button labeled "Submit" to submit the form.
+
+If a field is changed, the person object in the view should be updated.
--- src/app/app/exercise8/exercise8.ts
+++ src/app/app/exercise8/exercise8.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-exercise8',
-  imports: [],
+  imports: [
+    JsonPipe, ReactiveFormsModule
+  ],
   templateUrl: './exercise8.html',
-  styleUrl: './exercise8.css'
+  styleUrl: './exercise8.css',
+  standalone: true,
 })
-export class Exercise8 {
+export class Exercise8 implements OnInit, OnDestroy {
+  protected person = {
+    firstname: 'John',
+    lastname: 'Doe',
+    age: 30,
+    twitter: "@johndoe"
+  }
+  private mySubscription: Subscription = new Subscription();
 
+  protected personForm: FormGroup = new FormGroup({
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      age: new FormControl('', Validators.required),
+      twitter: new FormControl('', Validators.required)
+    }
+  )
+
+  protected updatePerson(): void {
+    // To show the changes only after clicking the button
+    // this.person = this.personForm.value;
+  }
+
+  ngOnInit(): void {
+    this.personForm.patchValue(this.person);
+    // To show instantly the changes without the need to click the button
+    this.mySubscription = this.personForm.valueChanges.subscribe(value => {
+      this.person = {
+        firstname: value.firstname,
+        lastname: value.lastname,
+        age: parseInt(value.age),
+        twitter: value.twitter
+      };
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.mySubscription) {
+      this.mySubscription.unsubscribe();
+    }
+  }
 }
